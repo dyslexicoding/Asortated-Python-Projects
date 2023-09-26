4a92ca4d127b9bdf97608447029b9b4da3e6f944
dyslexicoding <sami@taylorcrew.us>
2023-09-26 12:19:54 +0300

    not sure what is difrent today

--- slot machine/main.py
+++ slot machine/main.py
@@ -6,14 +6,76 @@
 # i don't suport gambling just for fun and utilizing random module
 
 import random
+from time import sleep
 
 
-MAX_LINES = 3 # max number of lines to play
+MAX_LINES = 3 # max number of lines to play 1 = top 2 = top and middle 3 = all
 MAX_BET = 100 # max bet amount
 MIN_BET = 1   # min bet amount
 
 ROWS = 3 # number of rows in slot machine
 COLS = 3 # number of columns in slot machine
+
+SybolCount = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+SybolValues = {
+    "A": 6,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def CheackWinings(Collums, lines, bet, Values):
+    Winnings = 0
+    WinningLines = []
+    for line in range(lines):
+        sybol = Collums[0][line]
+        for collum in Collums:
+            if collum[line] != sybol:
+                
+                break
+        else:
+            Winnings += Values[sybol] * bet
+            WinningLines.append(line+1)
+            
+    return Winnings, WinningLines
+        
+        
+def GetSlotMachineSpin(rows, cols, sybols):
+    AllSybols = []
+    for Sybol, SybolCount in sybols.items():
+        for _ in range(SybolCount):
+            AllSybols.append(Sybol)
+            
+    Collums = []
+    
+    for col in range(cols):
+        Collum = []
+        currentSybols = AllSybols.copy()
+        
+        for row in range(rows):
+            value = random.choice(currentSybols)
+            currentSybols.remove(value)
+            Collum.append(value)
+        
+        Collums.append(Collum)
+    
+    return Collums
+        
+
+def PrintSlotMachineSpin(Collums):
+    for row in range(len(Collums[0])):
+        # print("\n")
+        for i, col in enumerate(Collums):
+            if i != len(Collums) - 1:
+                print(col[row], end=" | ")
+            else:
+                print(col[row], end="")
+        print()
 
 def Deposit(): 
     # collect user input and return it
@@ -61,6 +123,10 @@ def GetBetAmount():
 
     return bet
 
+
+
+
+
 def main():
     # main function
     balance = Deposit()
@@ -79,5 +145,17 @@ def main():
     print ("You are betting on", lines, "lines.")
     print("you are betting $", Bet, "on each line.")
     print("Your total bet is $", TotalBet, ".")
+    
+    slots = GetSlotMachineSpin(ROWS, COLS, SybolCount)
+    print("Spinning...")
+    # sleep(1)
+    PrintSlotMachineSpin(slots)
+    
+    Winnings, WinningsLines = CheackWinings(slots, lines, Bet, SybolValues)
+    print("You won $", Winnings, ".")
+    print("You won on lines:", WinningsLines, ".")
+    
+    
+    
     
 main()
